deeb6b6ab64dadd63d81ce440bc37f744d30f5be
bertassoGabriel <gabrieljacintho868@hotmail.com>
2020-11-17 07:50:57 -0400

    Banco de dados H2, test profile, JPA

--- src/main/java/com/bertasso/webservices/WebservicesApplication.java
+++ src/main/java/com/bertasso/webservices/WebservicesApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class WebservicesApplication {
+public class WebServicesApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(WebservicesApplication.class, args);
+		SpringApplication.run(WebServicesApplication.class, args);
 	}
 
 }
