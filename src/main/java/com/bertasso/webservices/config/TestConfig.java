2545bf81e088124b044c49b9648591e50c025f03
bertassoGabriel <gabrieljacintho868@hotmail.com>
2020-11-18 07:28:30 -0400

    Entidade Payment, associação um para um

--- src/main/java/com/bertasso/webservices/config/TestConfig.java
+++ src/main/java/com/bertasso/webservices/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.bertasso.webservices.entities.Category;
 import com.bertasso.webservices.entities.Order;
 import com.bertasso.webservices.entities.OrderItem;
+import com.bertasso.webservices.entities.Payment;
 import com.bertasso.webservices.entities.Product;
 import com.bertasso.webservices.entities.User;
 import com.bertasso.webservices.entities.enums.OrderStatus;
@@ -80,6 +81,11 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 
 	}
 
